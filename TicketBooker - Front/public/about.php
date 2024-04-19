52a6188abd788ce4e642f501f8ffe7139a9af795
Brela1shala <brela.shala@student.uni-pr.edu>
2024-04-19 12:31:18 +0200

    changes

--- TicketBooker - Front/public/about.php
+++ TicketBooker - Front/public/about.php
@@ -35,10 +35,49 @@ $loggedIn = !empty($username); // Check if user is logged in
 				<a href="contact.php" class="link">Contact</a>
 				<a href="faq.php" class="link">FAQ</a>
 			</div>
+			<?php if ($loggedIn) : ?> 
 			<div class="right">
-				<a href="signup.php" class="link">Sign Up</a>
-				<a href="login.php" class="link" id="login">Log In</a>
+				<img id="profile-picture" src="assets/images/profiles/profile-picture-4.jpg" alt="" width="40" height="40"
+					style="border-radius: 50%;">
+				<p class="name"><?php echo $username; ?></p>
 			</div>
+
+			<div class="dropdown">
+				<div class="top">
+					<div class="info">
+						<img src="assets/images/profiles/profile-picture-4.jpg" alt="" width="50" height="50"
+							style="border-radius: 50%;">
+							<?php echo $username; ?>
+					</div>
+
+					<hr>
+				</div>
+
+				<div class="options">
+					<a href="profile.php" class="option">
+						<img src="assets/icons/profile.svg" alt="">
+						<p>Profile</p>
+					</a>
+					<a href="createTicket.php" class="option">
+						<img src="assets/icons/create.svg" alt="">
+						<p>Create Ticket</p>
+					</a>
+					<a href="editProfile.php" class="option">
+						<img src="assets/icons/settings.svg" alt="">
+						<p>Settings</p>
+					</a>
+					<a href="#" class="option">
+						<img src="assets/icons/logout.svg" alt="">
+						<p>Log out</p>
+					</a>
+				</div>
+			</div>
+			<?php else : ?> <!-- Show if user is not logged in -->
+            <div class="right">
+                <a href="signup.php" class="link">Sign Up</a>
+                <a href="login.php" class="link" id="login">Log In</a>
+            </div>
+			<?php endif; ?>
 			<i class="fa-solid fa-bars-staggered" id="burger-menu"></i>
 		</div>
 	</nav>
