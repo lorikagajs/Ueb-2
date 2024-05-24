f4c10378853965ef96c3a1f8263d7accc54fd477
lorikagajs <151840518+lorikagajs@users.noreply.github.com>
2024-05-24 13:23:18 +0200

    Added database script

---
+++ TicketBooker - Front/public/database/dbticketbooker.sql
@@ -0,0 +1,37 @@
+-- Create Database
+CREATE DATABASE IF NOT EXISTS ticketbooker;
+
+-- Use the newly created database
+USE ticketbooker;
+
+-- Create Users Table
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    user_type ENUM('Individual', 'Business') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create Tickets Table
+CREATE TABLE IF NOT EXISTS tickets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    event_time DATETIME NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    ticket_type ENUM('Movie', 'Travel', 'Concert') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create User Tickets Table
+CREATE TABLE IF NOT EXISTS user_tickets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    ticket_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (ticket_id) REFERENCES tickets(id),
+    added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
